178a928b39f016ca5b836fc109715856c642d7fb
FedericoBenedetti <s_bndfrc99s30l500c@itisurbino.it>
2018-03-31 22:49:26 +0200

    kernel: sched: fix merge derp

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1302,7 +1302,6 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	get_adjusted_cpumask(p, &adjusted_mask, new_mask);
 	new_mask = &adjusted_mask;
 
-	new_mask = adjust_cpumask(p, new_mask);
 	rq = task_rq_lock(p, &flags);
 
 	/*
